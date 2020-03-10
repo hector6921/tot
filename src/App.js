36398d528e2eefdbdbe90f9611ca0b915c70d0fc
unknown <hector_6921@hotmail.com>
2020-03-09 20:00:11 -0400

    needs work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import GetList from "./getlist";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    }
+  }
+  addTask = event => {
+  event.preventDefault();
+  this.setState({
+  todos: [...this.state.currentToDo, this.state.todos],
+})
+console.log("the task method was excuted", event);
+
+  };
+  handleChange = event => {
+    this.setState({
+    currentToDo: event.target.value,
+  })
+    console.log(event.target.value);
+  };
+
+
+  render(){
+    return(
+      <div className="container">
+      <form  onSubmit={this.addTask}>
+      <label htmlFor="taskName">Task Name: </label>
+      <input onChange={this.handleChange} name="taskName"
+      type="text" placeholder="Add todo here" />
+      <button type="submit" >Add task</button>
+      </form>
+      {this.state.todos.map((todo, index) => (
+          <GetList index ={index} todo={todo} />
+        ))
+      }
+      </div>
+    )
+  }
 }
-
-export default App;
+export default TodoList;
---
+++ src/getlist.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import "./app"
+
+Getlist = (props) => {
+var todo = props.todo;
+<ul>
+<li>{todo}</li>
+</ul>
+}
+export default Getlist;
